4a78af55d05d240c83b39e7c2754d01f694368ee
Paulo Fabiano Langer <paulofabiano@gmail.com>
2024-09-12 16:05:45 +0100

    chore: implement GetQuotation api function and parse external api response

--- server/api/api.go
+++ server/api/api.go
@@ -1,5 +1,12 @@
 package api
 
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"net/http"
+)
+
 type Quotation struct {
 	Code       string  `json:"code"`
 	CodeIn     string  `json:"codein"`
@@ -12,4 +19,31 @@ type Quotation struct {
 	Ask        float64 `json:"ask"`
 	Timestamp  string  `json:"timestamp"`
 	CreateDate string  `json:"create_date"`
+}
+
+func GetQuotation(ctx context.Context) (*Quotation, error) {
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "https://economia.awesomeapi.com.br/json/all/USD-BRL", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("API request failed")
+	}
+
+	var quotation Quotation
+	err = json.NewDecoder(resp.Body).Decode(&quotation)
+	if err != nil {
+		return nil, err
+	}
+
+	return &quotation, nil
 }
