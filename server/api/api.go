418ea0d7d04ad463fdfec05d16e69e75e209e3b9
Paulo Fabiano Langer <paulofabiano@gmail.com>
2024-09-12 18:16:16 +0100

    refactor: general refactorings and fixes

--- server/api/api.go
+++ server/api/api.go
@@ -8,17 +8,19 @@ import (
 )
 
 type Quotation struct {
-	Code       string  `json:"code"`
-	CodeIn     string  `json:"codein"`
-	Name       string  `json:"name"`
-	High       float64 `json:"high"`
-	Low        float64 `json:"low"`
-	VarBid     float64 `json:"varBid"`
-	PctChange  float64 `json:"pctChange"`
-	Bid        float64 `json:"bid"`
-	Ask        float64 `json:"ask"`
-	Timestamp  string  `json:"timestamp"`
-	CreateDate string  `json:"create_date"`
+	USD struct {
+		Code       string `json:"code"`
+		CodeIn     string `json:"codein"`
+		Name       string `json:"name"`
+		High       string `json:"high"`
+		Low        string `json:"low"`
+		VarBid     string `json:"varBid"`
+		PctChange  string `json:"pctChange"`
+		Bid        string `json:"bid"`
+		Ask        string `json:"ask"`
+		Timestamp  string `json:"timestamp"`
+		CreateDate string `json:"create_date"`
+	} `json:"USD"`
 }
 
 func GetQuotation(ctx context.Context) (*Quotation, error) {
--- server/database/database.go
+++ server/database/database.go
@@ -2,8 +2,9 @@ package database
 
 import (
 	"context"
-	"database/sql"
+	"strconv"
 
+	"github.com/paulofabiano/client-server-api/server/api"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -14,24 +15,27 @@ type Quotation struct {
 	gorm.Model
 }
 
-func InitDatabase() (*sql.DB, error) {
+func InitDatabase() (*gorm.DB, error) {
 	db, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
 
+	db.AutoMigrate(&Quotation{})
+
 	return db, nil
 }
 
-func SaveQuotation(ctx context.Context, db *sql.DB, quotation *Quotation) error {
-	stmt, err := db.Prepare("INSERT INTO quotations (bid) VALUES (?)")
+func SaveQuotation(ctx context.Context, db *gorm.DB, quotation *api.Quotation) error {
+	convertedBid, err := strconv.ParseFloat(quotation.USD.Bid, 64)
 	if err != nil {
 		return err
 	}
 
-	_, err = stmt.Exec(quotation.Bid)
-	if err != nil {
-		return err
+	q := Quotation{Bid: convertedBid}
+	result := db.WithContext(ctx).Create(&q)
+	if result.Error != nil {
+		return result.Error
 	}
 
 	return nil
